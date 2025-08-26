3dfaa4ad65b9b6bc180ca3c0859ac612ec1aa6a0
Emmanuel Martínez <emmanuel1407@hotmail.com>
2025-08-25 21:05:52 -0300

    Se crea el useProduct.

--- src/contexts/AppProvider.jsx
+++ src/contexts/AppProvider.jsx
@@ -1,13 +1,15 @@
 import PropTypes from "prop-types";
 import AppContext from "./AppContext";
+import { useProduct } from "@/hooks/useProduct";
 
 const AppProvider = (props) => {
     const { children } = props;
+    const productContext = useProduct();
 
     return (
         <AppContext.Provider
             value={{
-                greet: "Hola Mundo",
+                productContext,
             }}>
             {children}
         </AppContext.Provider>
--- src/pages/PageRoutes.jsx
+++ src/pages/PageRoutes.jsx
@@ -1,21 +1,27 @@
 import { Route, Routes } from "react-router-dom";
-import About from "./about/About";
-import Contact from "./contact/Contact";
-import Home from "./home/Home";
-import Product from "./product/Product";
-import Products from "./products/Products";
-import ShoppingCart from "./shopping-cart/ShoppingCart";
+
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./home/Home"));
+const About = lazy(() => import("./about/About"));
+const Contact = lazy(() => import("./contact/Contact"));
+const Products = lazy(() => import("./products/Products"));
+const Product = lazy(() => import("./product/Product"));
+const ShoppingCart = lazy(() => import("./shopping-cart/ShoppingCart"));
 
 const PageRoutes = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/product" element={<Product />} />
-            <Route path="/shopping-cart" element={<ShoppingCart />} />
-        </Routes>
+        <Suspense fallback="Cargando página...">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/product" element={<Product />} />
+                <Route path="/shopping-cart" element={<ShoppingCart />} />
+            </Routes>
+        </Suspense>
+
     );
 };
 
