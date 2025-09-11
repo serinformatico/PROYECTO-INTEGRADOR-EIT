abda94c76dbedae928e693c67146e7c318fdad2f
Montes Sergio <sgiaubasa@gmail.com>
2025-09-10 22:15:57 -0300

    Agrego Carrito

--- src/pages/shopping-cart/ShoppingCart.jsx
+++ src/pages/shopping-cart/ShoppingCart.jsx
@@ -1,10 +1,41 @@
 import { Text } from "@/components/texts";
 import "./shopping-cart.scss";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { useContext } from "react";
+import AppContext from "@/contexts/AppContext";
 
 const ShoppingCart = () => {
+    const { shoppingCartContext } = useContext(AppContext);
+    const { shoppingCart } = shoppingCartContext;
+
     return (
         <div className="shopping-cart">
             <Text variant="h2">Carrito</Text>
+
+            <Table>
+                <TableHead>
+                    <TableRow className="table__head">
+                        <TableCell align="left">Producto</TableCell>
+                        <TableCell align="right">Cant.</TableCell>
+                        <TableCell align="right">Precio</TableCell>
+                        <TableCell align="right">Importe</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {shoppingCart.articles?.map((article)=>(
+                        <TableRow key={article.id} className="table__body">
+                            <TableCell align="left">{article.title}</TableCell>
+                            <TableCell align="right">{article.quantity}</TableCell>
+                            <TableCell align="right">${article.price?.toFixed(2)}</TableCell>
+                            <TableCell align="right">${article.amount?.toFixed(2)}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+
+            <div className="table__footer">
+                <Text className="table__total" variant="p">Total: ${shoppingCart.totalAmount?.toFixed(2)}</Text>
+            </div>
         </div>
     );
 };
